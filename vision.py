7288d9fccc153dd988f99b315cf5da73b3227698
Warren-Partridge <maps@perdix.org>
2017-02-25 16:03:17 -0500

    Adds objectives.txt to list items to search for

--- vision.py
+++ vision.py
@@ -1,5 +1,6 @@
 import http.client, urllib.request, urllib.parse, urllib.error, base64
 import json
+import random
 
 headers = {
     # Request headers
@@ -14,19 +15,33 @@ params = urllib.parse.urlencode({
     'language': 'en',
 })
 
-try:
-    conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/vision/v1.0/analyze?%s" % params, '{"url":"https://www.what-dog.net/Images/faces2/scroll0015.jpg"}', headers)
-    response = conn.getresponse()
-    data = response.read().decode('utf-8')
-    data_json = json.loads(data)
-    data_json_tags = data_json["description"]["tags"]
-    data_json_descriptiontext = data_json["description"]["captions"][0]["text"]
+def imageRec(url):
+    #formattedUrl = '{"url":"' + url + '"}'
+    #formattedUrl = "'" + formattedUrl + "'"
+    #print(formattedUrl)
+    try:
+        conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
+        conn.request("POST", "/vision/v1.0/analyze?%s" % params, '{"url":"https://www.what-dog.net/Images/faces2/scroll0015.jpg"}', headers)
+        response = conn.getresponse()
+        data = response.read().decode('utf-8')
+        data_json = json.loads(data)
+        data_json_tags = data_json["description"]["tags"]
+        data_json_descriptiontext = data_json["description"]["captions"][0]["text"]
 
-    print(data_json_descriptiontext)
-    print("");
-    print(data_json_tags)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+        print(data_json_descriptiontext)
+        print("");
+        print(data_json_tags)
+        conn.close()
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
+def getObjective(afile):
+    return random.choice(list(open(afile)))
+
+
+def main():
+    print(getObjective("objectives.txt"))
+    imageRec("https://www.what-dog.net/Images/faces2/scroll0015.jpg")
+
+
+main()
