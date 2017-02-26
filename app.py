acf180009299e016354b49c6da9ea6fcef212fcc
Warren-Partridge <maps@perdix.org>
2017-02-25 23:30:46 -0500

    Adds test game logic for pi

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 from flask import Flask
-from run import getObjective, imageRec
+from run import getObjective, imageRec, checkIfPlayerIsGoodAtVideoGames
 from camera import takePicture
 
 app = Flask(__name__)
 
+currentObjective = getObjective("/home/pi/Desktop/Scavenger/objectives.txt")
+points = 0
+
 @app.route('/')
 def index():
-    msg = "Welcome to Scavenger! You have 0 points right now. Go take a picture of a " +\
-          getObjective("/home/pi/Desktop/Scavenger/objectives.txt") + " to get your first point."
+    msg = "Welcome to Scavenger! You have " + points + " points right now. Go take a picture of a " +\
+          currentObjective + " to get your first point."
     return '<html>'+msg+'<br><a href="/take">Take picture!</a>'
 
 @app.route('/take')
 def take():
     msg = 'Picture is going to be taken' 
     takePicture()
-    imageRec(None)
-    msg = msg + '<script>document.location="/"</script>'
-    return msg 
+    imageRecData = imageRec(None)
+    if(checkIfPlayerIsGoodAtVideoGames(imageRecData[1], currentObjective) == True):
+        points += 1
+        msg = "Nice " + currentObjective + " pic! Quality photography. " +\
+            "You got 1 point, making your new score " + str(points) + "."
+        currentObjective = getObjective("objectives.txt")
+        msg += "Now find something new; take a picture of a " + currentObjective + "!"
+        
+    return '<html><br><a href="/take">Take picture!</a>'+msg
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
